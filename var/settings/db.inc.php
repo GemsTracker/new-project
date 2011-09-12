1dcc75b1ee818e710a44782522b1e1ab06ed430c
Menno Dekker <mennodekker@users.sourceforge.net>
2011-09-12 12:46:41 +0000

    update for new project

---
+++ var/settings/db.inc.php
@@ -0,0 +1,10 @@
+<?php
+//-------------------------------------------------------------------------------------------------
+// Connect parameters for your project
+//-------------------------------------------------------------------------------------------------
+define ('HOST', 'localhost');
+define ('USER', 'new_project');
+define ('PASSWD', 'yourpassword');
+define ('DATABASE', 'new_project');
+
+?>
