2ff5ae5d1f64a743c5ae7303d055444e4aaa733a
Matijs de Jong <mjong@magnafacta.nl>
2012-07-19 11:12:13 +0000

    Used http://www.jslint.com/ to clean up and improve js code

--- htdocs/gems/js/autoSubmitForm.js
+++ htdocs/gems/js/autoSubmitForm.js
@@ -9,7 +9,8 @@ jQuery.widget("ui.autoSubmitForm", {
         // submitUrl: the request url
     },
 
-    _init: function() {
+    _init: function () {
+        "use strict";
         var self = this;
 
         /*
@@ -18,15 +19,16 @@ jQuery.widget("ui.autoSubmitForm", {
         console.log(this.options.targetId);   // Firebug console */
 
         // Bind the events
-        jQuery('input:text, select, textarea', this.element).keyup(function(e) {self.filter();});
-        jQuery('select', this.element).change(function(e) {self.filter();});
-        jQuery('input:checkbox, input:radio', this.element).click(function(e) {self.filter();});
+        jQuery('input:text, select, textarea', this.element).keyup(function (e) {self.filter(); });
+        jQuery('select', this.element).change(function (e) {self.filter(); });
+        jQuery('input:checkbox, input:radio', this.element).click(function (e) {self.filter(); });
 
         // Set the initial value
         this.lastQuery = this.value();
     },
 
     complete: function (request, status) {
+        "use strict";
         this.request = null;
 
         // Check for changes
@@ -37,48 +39,52 @@ jQuery.widget("ui.autoSubmitForm", {
         this.filter();
     },
 
-    destroy: function() {
-         if (this.request != null) {
-             this.request.abort();
-             this.request = null;
-         }
-    },
-
-    error: function (request, status) {
-        if (request.status === 401) {
-            location.href = location.href;
+    destroy: function () {
+        "use strict";
+        if (this.request !== null) {
+            this.request.abort();
+            this.request = null;
         }
     },
 
+    error: function (request, status) {
+        "use strict";
+        /*
+        if (request.status === 401) {
+            location.href = location.href;
+        } // */
+    },
+
     filter: function () {
+        "use strict";
+        var postData, self;
 
         //If we have a pending request and want to create a new one, cancel the first
         this.destroy();
 
-        if (this.request == null) {
+        if (this.request === null) {
 
             // var name = this.options.elementName ? this.options.elementName : this.element.attr('name');
 
-            var postData = this.value();
+            postData = this.value();
 
             if (this.options.targetId && this.options.submitUrl) {
                 // Prevent double dipping when e.g. the arrow keys were used.
-                if (jQuery.param(postData) != jQuery.param(this.lastQuery))
-                {
+                if (jQuery.param(postData) !== jQuery.param(this.lastQuery)) {
                     this.lastQuery = postData;
                     //console.log(postData);
 
                     //*
-                    var self = this;
+                    self = this;
                     this.request = jQuery.ajax({
                         url: this.options.submitUrl,
                         type: "POST",
                         dataType: "html",
                         data: postData,
-                        error: function(request, status, error) {self.error(request, status);},
-                        complete: function(request, status) {self.complete(request, status);},
-                        success: function(data, status, request) {self.success(data, status, request);}
-                        });
+                        error: function (request, status, error) {self.error(request, status); },
+                        complete: function (request, status) {self.complete(request, status); },
+                        success: function (data, status, request) {self.success(data, status, request); }
+                    });
                     // */
                 }
             }
@@ -86,11 +92,13 @@ jQuery.widget("ui.autoSubmitForm", {
     },
 
     success: function (data, status, request) {
+        "use strict";
         jQuery(this.options.targetId).html(data);
     },
 
     value: function () {
-        return $(this.element[0]).serializeArray();
+        "use strict";
+        return jQuery(this.element[0]).serializeArray();
     },
 
     lastQuery: null,
--- htdocs/gems/js/tableRowKeySelector.js
+++ htdocs/gems/js/tableRowKeySelector.js
@@ -6,42 +6,47 @@ jQuery.widget("ui.tableRowKeySelector", {
         currentClass: 'currentRow'
     },
 
-    _init: function() {
-        var self = this;
-        jQuery(document).keyup(function(e) {self.listener(e);});
+    _init: function () {
+        "use strict";
+        var find, link, keys, name, self, title;
+
+        self = this;
+        jQuery(document).keyup(function (e) {self.listener(e); });
 
         // Set the title attributes for the keys
-        var find, link, title;
-        var keys = {Down: '\u2193', End: 'End', Home: 'Home', Left: '\u2190', PgDn: 'PgDn', PgUp: 'PgUp', Right: '\u2192', Up: '\u2191'};
+        keys = {Down: '\u2193', End: 'End', Home: 'Home', Left: '\u2190', PgDn: 'PgDn', PgUp: 'PgUp', Right: '\u2192', Up: '\u2191'};
 
-        for (var name in keys) {
-            find  = 'a.key' + name +  '[href]';
-            title = keys[name];
-            link  = jQuery(find, this.element);
+        for (name in keys) {
+            if (keys.hasOwnProperty(name)) {
+                find  = 'a.key' + name +  '[href]';
+                title = keys[name];
+                link  = jQuery(find, this.element);
 
-            if (link.length > 0) {
-                if (! link.attr('title')) {
-                    link.attr('title', '[' + title + ']');
-                    // alert(link.attr('title'));
+                if (link.length > 0) {
+                    if (!link.attr('title')) {
+                        link.attr('title', '[' + title + ']');
+                        // alert(link.attr('title'));
+                    }
                 }
-            }
 
-            find = 'a.keyCtrl' + name +  '[href]';
-            link = jQuery(find, this.element);
+                find = 'a.keyCtrl' + name +  '[href]';
+                link = jQuery(find, this.element);
 
-            if (link.length > 0) {
-                if (! link.attr('title')) {
-                    link.attr('title', '[Ctrl^' + title + ']');
-                    // alert(link.attr('title'));
+                if (link.length > 0) {
+                    if (!link.attr('title')) {
+                        link.attr('title', '[Ctrl^' + title + ']');
+                        // alert(link.attr('title'));
+                    }
                 }
             }
         }
     },
 
-    listener: function(e) {
-         var currentItem, key, nextItem;
+    listener: function (e) {
+        "use strict";
+        var currentItem, key, link, nextItem;
 
-        if (e == null) {
+        if (e === null) {
             key = event.keyCode;
         } else { // mozilla
             key = e.which;
@@ -49,102 +54,102 @@ jQuery.widget("ui.tableRowKeySelector", {
 
         currentItem = jQuery('tr.' + this.options.currentClass + ':first', this.element);
 
-        switch(key) {
-            case 13: // Enter
-                if (currentItem.length > 0) {
-                    // this.element.after(jQuery('a[href]', currentItem).eq(0).attr('href') + ' enter<br/>');
-                    e.stopPropagation();
-                    location.href = jQuery('a[href]', currentItem).eq(0).attr('href');
-                    return;
-                }
-                break;
-                
-            case 33:  // PgUp
-                var link = jQuery('a.keyPgUp[href]', this.element);
-                if (link.length > 0) {
-                    e.stopPropagation();
-                    location.href = link.attr('href');
-                    return;
-                }
-                nextItem = jQuery('tbody > tr:first', this.element);
-                break;
+        switch (key) {
+        case 13: // Enter
+            if (currentItem.length > 0) {
+                // this.element.after(jQuery('a[href]', currentItem).eq(0).attr('href') + ' enter<br/>');
+                e.stopPropagation();
+                location.href = jQuery('a[href]', currentItem).eq(0).attr('href');
+                return;
+            }
+            break;
 
-            case 34:  // PgDn
-                var link = jQuery('a.keyPgDn[href]', this.element);
+        case 33:  // PgUp
+            link = jQuery('a.keyPgUp[href]', this.element);
+            if (link.length > 0) {
+                e.stopPropagation();
+                location.href = link.attr('href');
+                return;
+            }
+            nextItem = jQuery('tbody > tr:first', this.element);
+            break;
+
+        case 34:  // PgDn
+            link = jQuery('a.keyPgDn[href]', this.element);
+            if (link.length > 0) {
+                e.stopPropagation();
+                location.href = link.attr('href');
+                return;
+            }
+            nextItem = jQuery('tbody > tr:last', this.element);
+            break;
+
+        case 35:  // End
+            if (e.ctrlKey) {
+                nextItem = jQuery('tbody > tr:last', this.element);
+            } else {
+                link = jQuery('a.keyEnd[href]', this.element);
                 if (link.length > 0) {
                     e.stopPropagation();
                     location.href = link.attr('href');
                     return;
                 }
                 nextItem = jQuery('tbody > tr:last', this.element);
-                break;
+            }
+            break;
 
-            case 35:  // End
-                if (e.ctrlKey) {
-                    nextItem = jQuery('tbody > tr:last', this.element);
-                } else {
-                    var link = jQuery('a.keyEnd[href]', this.element);
-                    if (link.length > 0) {
-                        e.stopPropagation();
-                        location.href = link.attr('href');
-                        return;
-                    }
-                    nextItem = jQuery('tbody > tr:last', this.element);
+        case 36:  // Home
+            if (e.ctrlKey) {
+                nextItem = jQuery('tbody > tr:first', this.element);
+            } else {
+                link = jQuery('a.keyHome[href]', this.element);
+                if (link.length > 0) {
+                    e.stopPropagation();
+                    location.href = link.attr('href');
+                    return;
                 }
-                break;
-                      
-            case 36:  // Home
-                if (e.ctrlKey) {
-                    nextItem = jQuery('tbody > tr:first', this.element);
-                } else {
-                    var link = jQuery('a.keyHome[href]', this.element);
-                    if (link.length > 0) {
-                        e.stopPropagation();
-                        location.href = link.attr('href');
-                        return;
-                    }
-                    nextItem = jQuery('tbody > tr:first', this.element);
+                nextItem = jQuery('tbody > tr:first', this.element);
+            }
+            break;
+
+        case 38:  // Up
+            if (e.ctrlKey) {
+                link = jQuery('a.keyCtrlUp[href]', this.element);
+                if (link.length > 0) {
+                    e.stopPropagation();
+                    location.href = link.attr('href');
+                    return;
                 }
-                break;
+            }
 
-            case 38:  // Up
-                if (e.ctrlKey) {
-                    var link = jQuery('a.keyCtrlUp[href]', this.element);
-                    if (link.length > 0) {
-                        e.stopPropagation();
-                        location.href = link.attr('href');
-                        return;
-                    }
-                } 
+            if (currentItem.length === 0) {
+                nextItem = jQuery('tbody > tr:last', this.element);
+            } else {
+                nextItem = currentItem.prev();
+            }
+            // this.element.after('up<br/>');
+            break;
 
-                if (currentItem.length == 0) {
-                    nextItem = jQuery('tbody > tr:last', this.element);
-                } else {
-                    nextItem = currentItem.prev();
+        case 40: // down
+            if (e.ctrlKey) {
+                link = jQuery('a.keyCtrlDown[href]', this.element);
+                if (link.length > 0) {
+                    e.stopPropagation();
+                    location.href = link.attr('href');
+                    return;
                 }
-                // this.element.after('up<br/>');
-                break;
+            }
 
-            case 40: // down
-                if (e.ctrlKey) {
-                    var link = jQuery('a.keyCtrlDown[href]', this.element);
-                    if (link.length > 0) {
-                        e.stopPropagation();
-                        location.href = link.attr('href');
-                        return;
-                    }
-                } 
-
-                if (currentItem.length == 0) {
-                    nextItem = jQuery('tbody > tr:first', this.element);
-                } else {
-                    nextItem = currentItem.next();
-                }
-                // this.element.after('down<br/>');
-                break;
+            if (currentItem.length === 0) {
+                nextItem = jQuery('tbody > tr:first', this.element);
+            } else {
+                nextItem = currentItem.next();
+            }
+            // this.element.after('down<br/>');
+            break;
         }
 
-        if ((nextItem != null) && (nextItem.length > 0)) {
+        if ((nextItem !== null) && (nextItem.length > 0)) {
             if (currentItem.length) {
                 currentItem.removeClass(this.options.currentClass);
             }
@@ -152,4 +157,4 @@ jQuery.widget("ui.tableRowKeySelector", {
             nextItem.addClass(this.options.currentClass);
         }
     }
-}); 
+});
