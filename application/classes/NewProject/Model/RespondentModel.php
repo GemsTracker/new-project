e9204f1e2bc570b7b950fcd0c95f291514eb77d6
Matijs de Jong <mjong@magnafacta.nl>
2016-09-06 17:12:41 +0200

    Changed file comments to current standard

--- application/classes/NewProject/Model/RespondentModel.php
+++ application/classes/NewProject/Model/RespondentModel.php
@@ -1,51 +1,31 @@
 <?php
 
 /**
- * Copyright (c) 2012, Erasmus MC
- * All rights reserved.
+ * Default Gemstracker project Respondent Model
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of Erasmus MC nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
+ * Normal practice is to change all NewProject references to the project name
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
- * @package    Gems
+ * @package    NewProject
  * @subpackage Model
- * @author     Matijs de Jong <mjong@magnafacta.nl>
- * @copyright  Copyright (c) 2012 Erasmus MC
- * @license    New BSD License
- * @version    $id: RespondentModel.php 203 2012-01-01t 12:51:32Z matijs $
- */
-
-/**
- * Default inmplementation of Dutch respondent model
- *
- * @package    Gems
- * @subpackage Model
- * @copyright  Copyright (c) 2012 Erasmus MC
- * @license    New BSD License
- * @since      Class available since version 1.6
+ * @copyright  Copyright (c) 2016 Project
+ * @license    No free license, do not copy
  */
 class NewProject_Model_RespondentModel extends \Gems_Model_RespondentNlModel
 {
+   /**
+     * Set those settings needed for the browse display
+     *
+     * @return \Gems_Model_RespondentModel
+     * /
+    public function applyBrowseSettings()
+    {
+        parent::applyBrowseSettings();
+
+        $this->applyProjectSettings();
+
+        return $this;
+    }
+
     /**
      * Set those settings needed for the detailed display
      *
@@ -55,7 +35,7 @@ class NewProject_Model_RespondentModel extends \Gems_Model_RespondentNlModel
     {
         parent::applyDetailSettings();
 
-        // $this->setIfExists('grs_phone_2', 'label', $this->_('Mobile phone'));
+        $this->applyProjectSettings();
 
         return $this;
     }
@@ -70,6 +50,32 @@ class NewProject_Model_RespondentModel extends \Gems_Model_RespondentNlModel
     {
         parent::applyEditSettings($create);
 
+        // parent::applyEditSettings($create) calls parent::applyDetailSettings() which may call applyProjectSettings()
+        // $this->applyProjectSettings();
+
         return $this;
+    }
+
+    /**
+     * Set those settings needed for the detailed display
+     *
+     * @return \Gems_Model_RespondentModel
+     * /
+    protected function applyProjectSettings()
+    {
+        //$this->del();
+
+        $echo = array(
+            'Yes' => $this->_('Yes'),
+            'No'  => $this->_('No'),
+            ''    => $this->_('Unknown'),
+        );
+        $this->set('gr2o_echo', 'label', $this->_('Echo'),
+                'description', $this->_('Patient consents to echo'),
+                'elementClass', 'Radio',
+                'multiOptions', $echo,
+                'order', $this->getOrder('gr2o_consent') + 1,
+                'separator', ' '
+                );
     } // */
 }
